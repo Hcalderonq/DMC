d5a12b99906d349b17e6e609b9696f1d984f6c9a
Hcalderonq <94331593+Hcalderonq@users.noreply.github.com>
2023-11-01 07:12:53 -0500

    V2.0

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-console.clear();
 import { createServer } from 'http';
 import { URL } from 'url';
 import axios from 'axios';
+import fs from 'fs';
 
 const httpServer = createServer(async (req, res) => {
-    const myURL = new URL(req.url, `http://${req.headers.host}`);
 
-    const param1 = myURL.searchParams.get('param1');  // Supongo que este será el COD_CIA
-    const param2 = myURL.searchParams.get('param2');  // Supongo que este será el ID_FAC
+    console.log('URL:', req.url);
+    console.log('Host:', req.headers.host);
+    console.log('Encabezados:', req.headers); // Registrar todos los encabezados de la solicitud
+
+    const myURL = new URL(req.url, `http://${req.headers.host}`);
+    const param1 = myURL.searchParams.get('param1');
+    const param2 = myURL.searchParams.get('param2');
 
     console.log('Param1:', param1);
     console.log('Param2:', param2);
@@ -15,29 +19,44 @@ const httpServer = createServer(async (req, res) => {
     if (req.method === 'POST') {
         let body = '';
         req.on('data', chunk => {
-            body += chunk.toString();
+            body += chunk.toString('utf8'); // Se asegura de que el encoding sea UTF-8
         });
 
         req.on('end', async () => {
+            console.log('Cuerpo en bruto:', body); // Registrar el cuerpo en bruto recibido
+
             try {
                 const jsonData = JSON.parse(body);
                 console.log('JSON recibido:', jsonData);
 
-                // Realizar la solicitud al API
+                // Escribe el JSON entrante en un archivo
+                fs.writeFileSync('json_entrante.txt', JSON.stringify(jsonData, null, 2));
+
                 const apiUrl = `https://apex7.revion.com/ords/lcorp/DMC/FACTURAS/EMI/${param1}/${param2}`;
-                const apiResponse = await axios.post(apiUrl, jsonData);
+                // Enviar el cuerpo directamente sin procesamiento adicional
+                const apiResponse = await axios.post(apiUrl, body, {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
                 console.log('Respuesta del API:', apiResponse.data);
 
-                res.end('Solicitud procesada');
+                // Escribe el JSON saliente en un archivo
+                fs.writeFileSync('json_saliente.txt', JSON.stringify(apiResponse.data, null, 2));
+
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(apiResponse.data));
+
             } catch (e) {
-                console.error('Error:', e);
+                console.error('Error detallado:', e);
                 res.writeHead(400, { 'Content-Type': 'text/plain' });
-                res.end('Solicitud no válida');
+                res.end('Solicitud no válida: ' + e.message);
             }
         });
+
     } else {
         res.end('Esperando solicitud POST');
     }
 });
 
-httpServer.listen(3000);
+httpServer.listen(8080);
